4e96abb38341245f05ce427842836de2d24eb90f
AdvTechnoKing <AdvancedTechnoKing@gmail.com>
2019-05-05 13:57:27 +0300

    vk.com/@devgg-react-create-app

---
+++ precache-manifest.320f52bd4640a5ccd65646405e0e7617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "04d847c493c01aba187e",
+    "url": "/static/js/main.04d847c4.chunk.js"
+  },
+  {
+    "revision": "896c6fc7b598bd5cfdab",
+    "url": "/static/js/2.896c6fc7.chunk.js"
+  },
+  {
+    "revision": "04d847c493c01aba187e",
+    "url": "/static/css/main.a67ef5f9.chunk.css"
+  },
+  {
+    "revision": "896c6fc7b598bd5cfdab",
+    "url": "/static/css/2.63fcc18b.chunk.css"
+  },
+  {
+    "revision": "262c42ba86e16852aa5f75125de58f52",
+    "url": "/index.html"
+  }
+];
